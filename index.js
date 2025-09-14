eb524e37c6c8b6414aba30748dd8a167ba0cd85c
Lucas Larson <lucaslarson25@gmail.com>
2025-09-14 14:01:09 -0700

    Main page styled, created new post modal form.

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const app = express();
 
 app.use(express.json());
 
+app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'))
+
+posts = [{title: 'First Post!', author: 'Lucas Larson', text: 'Check out this new post I made! Try to make your own too!', timestamp: Date.now()}]
+
 app.get('/', (req, res) => {
-    res.render("index.ejs")
+    res.render("index.ejs", {posts})
 });
 
 app.listen(3000, () => {
